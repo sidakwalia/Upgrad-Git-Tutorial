8e36eec65ffcf4f907f23014684fecefb7c2ad94
inderpreet.singh <inderpreet.singh@algoscale.com>
2022-05-29 18:04:24 +0530

    Rachana code for subtraction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("hello github welcome to the session")
+from subtract1 import *
 
 def calculator(num1,num2):
-    return num1+num2
+    return num1+num2-subtract1(num1+num2)
--- subtract.py
+++ subtract.py
@@ -1,2 +1,5 @@
-def subtract(num1,num2):
+def subtract1(num1,num2):
     return num1-num2
+
+def multiply(num1,num2)
+    return num1*num2
